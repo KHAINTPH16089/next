0ddad8f0fb809f77260a7bd30e7a793cac910a2f
khaintph16089 <khaintph16089@fpt.edu.vn>
2022-07-19 15:43:12 +0700

    hooks product

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,15 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Layout from '../component/Layout'
 import { AppPropsWithLayout } from '../models/layout'
+import { SWRConfig } from 'swr';
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const LayoutWrapper = Component.Layout ?? Layout;
-  return <LayoutWrapper><Component {...pageProps} /></LayoutWrapper>
+  return <LayoutWrapper>
+      <SWRConfig value={{fetcher: async (url) => {}}}>
+        <Component {...pageProps} />
+      </SWRConfig>
+    </LayoutWrapper>
 }
 
 export default MyApp
